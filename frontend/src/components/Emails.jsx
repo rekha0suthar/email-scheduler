3ce41f98e33d3f758b31425c7928ff4fcb284213
rekha0suthar <rekha0suthar@gmail.com>
2024-12-12 20:22:14 +0530

    loading message added

--- frontend/src/components/Emails.jsx
+++ frontend/src/components/Emails.jsx
@@ -2,36 +2,42 @@ import React, { useContext, useEffect } from 'react';
 import { Context } from '../Context';
 
 const Emails = () => {
-  const { emails, fetchScheduledEmails } = useContext(Context);
+  const { loading, emails, fetchScheduledEmails } = useContext(Context);
 
   useEffect(() => {
     fetchScheduledEmails();
   }, []);
   return (
     <div className="emails-container">
-      {emails.map((email) => (
-        <div className="email-wrapper">
-          <span>
-            <h3>Subject: {email.subject}</h3>
-            <p
-              style={{
-                color:
-                  email.status === 'pending'
-                    ? 'orange'
-                    : email.status === 'sent'
-                    ? 'green'
-                    : 'red',
-              }}
-            >
-              Status: {email.status}
-            </p>
-          </span>
+      {loading && <p className="dummy-message">Loading emails .....</p>}
+      {!loading &&
+        emails.length > 0 &&
+        emails.map((email) => (
+          <div className="email-wrapper">
+            <span>
+              <h3>Subject: {email.subject}</h3>
+              <p
+                style={{
+                  color:
+                    email.status === 'pending'
+                      ? 'orange'
+                      : email.status === 'sent'
+                      ? 'green'
+                      : 'red',
+                }}
+              >
+                Status: {email.status}
+              </p>
+            </span>
 
-          <p>From: {email.senderEmail}</p>
-          <p>To: {email.receiverEmail}</p>
-          <p>Message: {email.body}</p>
-        </div>
-      ))}
+            <p>From: {email.senderEmail}</p>
+            <p>To: {email.receiverEmail}</p>
+            <p>Message: {email.body}</p>
+          </div>
+        ))}
+      {emails.length === 0 && !loading && (
+        <p className="dummy-message">No emails scheduled</p>
+      )}
     </div>
   );
 };
